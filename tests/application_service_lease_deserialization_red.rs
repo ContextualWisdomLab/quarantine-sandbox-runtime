7d6eeaeb892f718f132523762a3196970640133d
Seongho Bae <me@seonghobae.me>
2026-09-08 20:05:39 +0900

    style(test): apply rustfmt import order to lease RED

--- tests/application_service_lease_deserialization_red.rs
+++ tests/application_service_lease_deserialization_red.rs
@@ -6,7 +6,7 @@
 use quarantine_sandbox_runtime::{
     ApplicationServiceLease, IsolationAttestation, ServiceEndpoint,
 };
-use serde_json::{Value, json};
+use serde_json::{json, Value};
 
 fn valid_attestation() -> Value {
     json!({
