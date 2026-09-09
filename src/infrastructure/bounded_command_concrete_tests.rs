684bfacdac2ee77ddeec8a3e7ac6cddf0c1581bb
Seongho Bae <me@seonghobae.me>
2026-09-09 18:32:45 +0900

    test(runtime): update concrete spawn expectation

--- src/infrastructure/bounded_command_concrete_tests.rs
+++ src/infrastructure/bounded_command_concrete_tests.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, time::Duration};
+use std::{io::ErrorKind, path::Path, time::Duration};
 
 use super::bounded_command::{BoundedCommandError, BoundedCommandRunner};
 
@@ -52,7 +52,7 @@ fn concrete_child_spawn_failure_is_typed() {
         .run(Path::new("/definitely-missing-qsr-command"), &[])
         .err();
 
-    assert_eq!(error, Some(BoundedCommandError::Spawn));
+    assert_eq!(error, Some(BoundedCommandError::Spawn(ErrorKind::NotFound)));
 }
 
 #[test]
