e9519d6759f17b06fd7ce0a7c49c0800a0110126
Seongho Bae <me@seonghobae.me>
2026-09-10 19:27:36 +0900

    test(runtime): keep Podman 4.9 fixture resource-bound

--- tests/podman_command_execution_podman_4_9_compat_red.rs
+++ tests/podman_command_execution_podman_4_9_compat_red.rs
@@ -101,7 +101,7 @@ case "${1:-}:${2:-}" in
     :
     ;;
   container:inspect)
-    printf '%s\n' '[{"Id":"fake-command-container-id","AppArmorProfile":"containers-default","ProcessLabel":"","EffectiveCaps":[],"BoundingCaps":[],"Config":{"User":"65532:65532"},"HostConfig":{"ReadonlyRootfs":true,"Privileged":false,"SecurityOpt":["no-new-privileges"],"UsernsMode":"auto","Annotations":{},"PidMode":"private","IpcMode":"none","NetworkMode":"none","UTSMode":"private","CgroupMode":"private","Memory":268435456,"NanoCpus":1000000000,"PidsLimit":16}}]'
+    printf '%s\n' '[{"Id":"fake-command-container-id","AppArmorProfile":"containers-default","ProcessLabel":"","EffectiveCaps":[],"BoundingCaps":[],"Config":{"User":"65532:65532","Timeout":20},"HostConfig":{"ReadonlyRootfs":true,"Privileged":false,"SecurityOpt":["no-new-privileges"],"UsernsMode":"auto","Annotations":{},"PidMode":"private","IpcMode":"none","NetworkMode":"none","UTSMode":"private","CgroupMode":"private","Memory":268435456,"NanoCpus":1000000000,"PidsLimit":16,"Tmpfs":{"/tmp":"rw,noexec,nosuid,nodev,size=16777216"}}}]'
     ;;
   top:*)
     printf '%s\n' 'PID SECCOMP CAPEFF CAPBND CAPINH CAPPRM CAPAMB LABEL'
